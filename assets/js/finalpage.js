a10fe0be981851e4906885737f093c37c08d9b02
Govnor Payne <govnorpayne@gmail.com>
2022-03-23 17:39:17 -0500

    got director name and actors names

--- assets/js/finalpage.js
+++ assets/js/finalpage.js
@@ -61,6 +61,31 @@ var displaySelectedTitle = function (genre) {
           document.getElementById("feature-hero").style.backgroundImage =
             "url(" + featureBackdropUrl;
           // pick up here*************
+          var castURL =
+            "https://api.themoviedb.org/3/movie/" +
+            featureImbdId +
+            "/credits?api_key=921ba47b5c4b85bc48dd2db9202db1be&language=en-US";
+          fetch(castURL).then(function (response) {
+            if (response.ok) {
+              response.json().then(function (data) {
+                console.log(data);
+                var director = Object.values(data.crew[1])[4];
+                var cast =
+                  Object.values(data.cast[0])[4] +
+                  " and " +
+                  Object.values(data.cast[1])[4];
+                // var cast1 = Object.values(data.cast[1])[4];
+
+                // console.log(cast1);
+                console.log(cast);
+                console.log(director);
+                document.getElementById("feature-director").textContent =
+                  director;
+                document.getElementById("feature-actors").textContent = cast;
+                // document.getElementById("feature-actors").textContent = cast1;
+              });
+            }
+          });
         });
       } else {
         alert("error: " + response.statusText);
