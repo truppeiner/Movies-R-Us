434f2179ce24e5da2c5c5405e17a4c1ec2274f8b
Govnor Payne <govnorpayne@gmail.com>
2022-03-24 07:28:59 -0500

    added final js

--- assets/js/mainScript.js
+++ assets/js/mainScript.js
@@ -1,11 +1,3 @@
-
-var actionButtonEl = document.querySelector('#genre-buttons');
-var genreSearchTerm = document.querySelector('#genre-search-term');
-var genreResultsContainerEl = document.querySelector('#genre-results-container');
-var titleBoxEl = document.querySelector('#title-box');
-
-
-var buttonClickHandler = function(event) {
 var actionButtonEl = document.querySelector("#genre-buttons");
 var genreSearchTerm = document.querySelector("#genre-search-term");
 var genreResultsContainerEl = document.querySelector(
@@ -14,37 +6,20 @@ var genreResultsContainerEl = document.querySelector(
 var titleBoxEl = document.querySelector("#title-box");
 
 var buttonClickHandler = function (event) {
+  var actionButtonEl = document.querySelector("#genre-buttons");
+  var genreSearchTerm = document.querySelector("#genre-search-term");
+  var genreResultsContainerEl = document.querySelector(
+    "#genre-results-container"
+  );
+};
+var titleBoxEl = document.querySelector("#title-box");
+
+var buttonClickHandler = function (event) {
   // get the genre attribute from the clicked element
   var genre = event.target.getAttribute("genre");
   if (genre) {
-    window.location.href ="./common/secondpage.html?genre=" + genre;
-    window.location.href = "./secondpage.html?genre=" + genre;
+    window.location.href = "./common/secondpage.html?genre=" + genre;
   }
 };
 
 actionButtonEl.addEventListener("click", buttonClickHandler);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
