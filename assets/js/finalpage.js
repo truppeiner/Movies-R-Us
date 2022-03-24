ff79c0569b59537fb8b21a833eab933a9bca8a7f
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-03-24 10:06:02 -0500

    trailers

--- assets/js/finalpage.js
+++ assets/js/finalpage.js
@@ -57,23 +57,23 @@ var displaySelectedTitle = function(genre) {
           document.getElementById("trailer-link").setAttribute("href", featureTrailerUrl);
           document.getElementById("trailer-link").setAttribute("target", "_blank");
 
-      var castURL = "https://api.themoviedb.org/3/movie/" + featureImbdId + "/credits?api_key=921ba47b5c4b85bc48dd2db9202db1be&language=en-US"; 
-      fetch(castURL).then(function (response) {
-         if (response.ok) { 
-           response.json().then(function (data) { 
-           console.log(data); 
-           var director = Object.values(data.crew[1])[4]; 
-           var cast = Object.values(data.cast[0])[4] + " and " + Object.values(data.cast[1])[4]; 
-           // var cast1 = Object.values(data.cast[1])[4];
-           // console.log(cast1);
-           console.log(cast);
-           console.log(director);
-           document.getElementById("feature-director").textContent = director;
-           document.getElementById("feature-actors").textContent = cast;
-          // document.getElementById("feature-actors").textContent = cast1;
-          });
-    }
-  });
+          var castURL = "https://api.themoviedb.org/3/movie/" + featureImbdId + "/credits?api_key=921ba47b5c4b85bc48dd2db9202db1be&language=en-US"; 
+          fetch(castURL).then(function (response) {
+          if (response.ok) { 
+            response.json().then(function (data) { 
+            console.log(data); 
+            var director = Object.values(data.crew[1])[4]; 
+            var cast = Object.values(data.cast[0])[4] + " and " + Object.values(data.cast[1])[4]; 
+            // var cast1 = Object.values(data.cast[1])[4];
+            // console.log(cast1);
+            console.log(cast);
+            console.log(director);
+            document.getElementById("feature-director").textContent = director;
+            document.getElementById("feature-actors").textContent = cast;
+           // document.getElementById("feature-actors").textContent = cast1;
+           });
+          }
+        });
       })
     } else {
       alert('error: ' + response.statusText);
@@ -82,9 +82,6 @@ var displaySelectedTitle = function(genre) {
   .catch(function(error) {
       console.log(error);
        });
-
-  
-
 };
 
 displaySelectedTitle()
