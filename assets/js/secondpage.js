a499c42c24d8eb0030e882da23cfe2aecc8b4203
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-03-24 17:36:36 -0500

    2ndpagejs

--- assets/js/secondpage.js
+++ assets/js/secondpage.js
@@ -1,68 +1,122 @@
+var resultsBox = document.querySelector("#results-box");
+var detailsButtonEl = document.querySelector('#title-card-box');
+
 var searchSelectedGenre = function(genre) {
     var url = new URL(window.location);
-    var genre = url.searchParams.get("genre");
+    var genre = url.searchParams.get("genre") 
     console.log(genre);
+    if (genre === "1") {
+        let selectedGenre = "Action";
+        document.getElementById("selected-genre").textContent=selectedGenre;
+    } else if (genre === "4") {
+        let selectedGenre = "Comedy";
+        document.getElementById("selected-genre").textContent=selectedGenre;
+    } else if (genre === "6") {
+        let selectedGenre = "Documentary";
+        document.getElementById("selected-genre").textContent=selectedGenre;
+    } else if (genre === "7") {
+        let selectedGenre = "Drama";
+        document.getElementById("selected-genre").textContent=selectedGenre;
+    } else if (genre === "40") {
+        let selectedGenre = "Fantasy";
+        document.getElementById("selected-genre").textContent=selectedGenre;
+    } else if (genre === "10") {
+        let selectedGenre = "History";
+        document.getElementById("selected-genre").textContent=selectedGenre;
+    } else if (genre === "11") {
+        let selectedGenre = "Horror";
+        document.getElementById("selected-genre").textContent=selectedGenre;
+    } else if (genre === "32") {
+        let selectedGenre = "Musical";
+        document.getElementById("selected-genre").textContent=selectedGenre;
+      } else if (genre === "13") {
+        let selectedGenre = "Mystery";
+        document.getElementById("selected-genre").textContent=selectedGenre;
+    } else if (genre === "14") {
+        let selectedGenre = "Romance";
+        document.getElementById("selected-genre").textContent=selectedGenre;       
+    } else if (genre === "15") {
+        let selectedGenre = "Science-Fiction"
+        document.getElementById("selected-genre").textContent=selectedGenre;
+    } else if (genre === "17") {
+        let selectedGenre = "Thriller"
+        document.getElementById("selected-genre").textContent=selectedGenre;
+    } else if (genre === "18") {
+        let selectedGenre = "War"
+        document.getElementById("selected-genre").textContent=selectedGenre;
+    }
     // format the watchmode api url for genre title list
-    var watchModeGenreUrl = 'https://api.watchmode.com/v1/list-titles?genres=' + genre + '&limit=1&apiKey=ZAhWH7fs7akLihCUWHsN61BnLhW8cRtGdRZea30D';
+    var watchModeGenreUrl = 'https://api.watchmode.com/v1/list-titles?genres=' + genre + '&limit=100&apiKey=WIu3mU2xnsXe9BTf7WlTqfAmFnw3uwR5kTG1RtbB';
     fetch(watchModeGenreUrl).then(function(response) {
       //request successful
       if (response.ok) {
         response.json().then(function(data) {
-          displayGenreResults(data.titles, genre);
-         // console.log(data.titles);
+            console.log(data);
+        displayGenreResults(data, data.titles);
+            console.log(data.titles);
+            console.log(data.titles[0].id);
+            console.log(data.titles[0].title);
+            console.log(data.titles[0].year);
         });
       } else {
         alert('error: ' + response.statusText);
-      }
-    });
-  };
+    }
+    })
+    .catch(function(error) {
+        console.log(error);
+         });
+    };
 
-  var displayGenreResults = function(titles, genre) {
-    //console.log(titles);
-    console.log(genre)
+  var displayGenreResults = function(titles) {
+    console.log(titles);
+    console.log(titles.titles[0].id);
+    console.log(titles.titles[0].title);
+    console.log(titles.titles[0].year);
+
     // check if api returned any titles
-    if (titles.length === 0) {
-      genreResultsContainerEl.textContent = 'no titles found';
+    if (titles.titles.length === 0) {
+      console.log('no titles found');
       return;
     }
-    //genreSearchTerm.textContent = genre;
-<<<<<<< HEAD
-  
-  
-    // loop over titles
-    for (var i = 0; i < titles.length; i++) {
     
-=======
-
-
     // loop over titles
-    for (var i = 0; i < titles.length; i++) {
-
->>>>>>> 821787bc060ee74f78603607d3a5f39863c59a1b
-      var titleName = titles[i].title; 
-      var titleId = titles[i].id;
-      var watchModeTitleIdUrl ='https://api.watchmode.com/v1/title/'+ titleId + '/details?append_to_response=sources&apiKey=ZAhWH7fs7akLihCUWHsN61BnLhW8cRtGdRZea30D';
-      fetch(watchModeTitleIdUrl).then(function(response) {
-        response.json().then(function(data){
-          console.log(data);
-          console.log(titleName);
-          console.log(data.sources);
-          console.log(data.poster);   
-          console.log(data.plot_overview);
-          console.log(data.year);
-        })
-      })
-    }
-<<<<<<< HEAD
+    for (var i = 0; i < titles.titles.length; i++) {
+      //console.log(titles.titles);
+      //console.log(titles.titles[0].id);
    
-  };
-  
+      var titleName = titles.titles[i].title; 
+      var titleId = titles.titles[i].id;
+      var titleYear = titles.titles[i].year;
+      //console.log(titleName);
+      
+      for (let i = 0; i < titles.titles.length; i++) {
+        var dynaTitleCard = document.createElement("div"); 
+        dynaTitleCard.setAttribute("id", "title-card")
+        dynaTitleCard.classList="card column is-one-fifth"
+        dynaTitleCard.innerHTML +='<h3 class="card-header" "movie-title" id="selection-title'+i+'"></h3><span class="year" id="release-year'+i+'"></span><button id="details-button'+i+'" class="btn"></button>'                                          
+        
+        var titleCardBox = document.querySelector("#title-card-box")
+        document.getElementById("title-card-box");
+        titleCardBox.appendChild(dynaTitleCard);
+        document.getElementById("selection-title"+i).textContent=titles.titles[i].title;
+        document.getElementById("release-year"+i).textContent=titles.titles[i].year
+        document.getElementById("details-button"+i).textContent="Title Details";
+        document.getElementById("details-button"+i).setAttribute("title-id", titles.titles[i].id); 
+      }
+      break;
+    }
+}
 
-  searchSelectedGenre()
-=======
+var detailsButtonClickHandler = function(event) {
+  // get the title id attribute from the clicked element
+  var featureTitleId = event.target.getAttribute('title-id');
+  //console.log(id);
+  if (featureTitleId) {
+    window.location.href ="./finalpage.html?featureTitleId=" + featureTitleId;
+  }
 
-  };
+};
 
+searchSelectedGenre()
 
-  searchSelectedGenre() 
->>>>>>> 821787bc060ee74f78603607d3a5f39863c59a1b
+detailsButtonEl.addEventListener("click", detailsButtonClickHandler);
