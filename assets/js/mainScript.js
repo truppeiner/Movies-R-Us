b763d45d8d4b441d359d731d647726b3206f6500
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-03-24 17:34:00 -0500

    main js

--- assets/js/mainScript.js
+++ assets/js/mainScript.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 821787bc060ee74f78603607d3a5f39863c59a1b
 var actionButtonEl = document.querySelector('#genre-buttons');
 var genreSearchTerm = document.querySelector('#genre-search-term');
 var genreResultsContainerEl = document.querySelector('#genre-results-container');
@@ -12,15 +8,11 @@ var buttonClickHandler = function(event) {
   // get the genre attribute from the clicked element
   var genre = event.target.getAttribute('genre');
   if (genre) {
-    window.location.href ="./common/secondpage.html?genre=" + genre
+    window.location.href ="./common/secondpage.html?genre=" + genre;
   }
 };
 
-
-
-
 actionButtonEl.addEventListener("click", buttonClickHandler);
-<<<<<<< HEAD
 
 
 
@@ -45,5 +37,3 @@ actionButtonEl.addEventListener("click", buttonClickHandler);
 
 
 
-=======
->>>>>>> 821787bc060ee74f78603607d3a5f39863c59a1b
