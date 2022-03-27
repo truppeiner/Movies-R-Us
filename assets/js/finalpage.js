306bc14248c7dc7dd159528e02437aa592591abe
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-03-27 16:52:20 -0500

    final page js

--- assets/js/finalpage.js
+++ assets/js/finalpage.js
@@ -2,7 +2,6 @@ var displaySelectedTitle = function(genre) {
   var url = new URL(window.location);
   var selectedTitleId = url.searchParams.get("featureTitleId") 
   console.log(selectedTitleId);
-
   // format the watchmode api url for selected title details
   var selectedTitleApiUrl = 'https://api.watchmode.com/v1/title/' + selectedTitleId + '/details/?append_to_response=sources&apiKey=3vWxYKPiq0kntxPSw8B2hMi3OiBvlMzGkqZPSj6R';
   fetch(selectedTitleApiUrl).then(function(response) {
@@ -22,7 +21,8 @@ var displaySelectedTitle = function(genre) {
           console.log(data.backdrop);
           console.log(data.sources);
           console.log(data.trailer_thumbnail);
-          // store desired data in variables
+          // store desired data in variables and append to final page display
+          //show type
           if (data.type ==="tv_series") {
             let featureType = "TV Series"
             document.getElementById("feature-type").textContent=featureType;
@@ -30,6 +30,52 @@ var displaySelectedTitle = function(genre) {
             let featureType = "Movie"
             document.getElementById("feature-type").textContent=featureType;
           }
+
+         // streaming source url links for selected titled
+          const sources = data.sources;
+          const disneyPlus = sources.filter(source => source.source_id === 372);
+          if (disneyPlus.length >= 1) {
+            let disneyTitleUrl= disneyPlus[0].web_url;
+            console.log(disneyTitleUrl);
+            document.getElementById("streaming-links").innerHTML +='<a href="" target="_blank" id="disney-plus-link"><h4 id="disney-plus">Disney+</h4></a>' 
+            document.getElementById("disney-plus-link").setAttribute("href", disneyTitleUrl);
+          } 
+          
+          const netflix = sources.filter(source => source.source_id === 203)
+          if (netflix.length >= 1) {
+           let netflixTitleUrl= netflix[0].web_url;
+           document.getElementById("streaming-links").innerHTML +='<a href="" target="_blank" id="netflix-link"><h4 id="netflix" >Netflix</h4></a>'
+           document.getElementById("netflix-link").setAttribute("href", netflixTitleUrl);
+           } 
+
+          const hulu = sources.filter(source => source.source_id === 157)
+          if (hulu.length >= 1) {
+          let huluTitleUrl = hulu[0].web_url;
+          document.getElementById("streaming-links").innerHTML +='<a href=" " target="_blank" id="hulu-link"><h4 id="hulu">Hulu</h4></a>'
+          document.getElementById("hulu-link").setAttribute("href", huluTitleUrl);
+          } 
+
+          const amazonPrime = sources.filter(source => source.source_id === 26)
+          if (amazonPrime.length >= 1) {
+             let amazonPrimeTitleUrl = amazonPrime[0].web_url;
+             document.getElementById("streaming-links").innerHTML +='<a href=" " target="_blank" id="amazon-link"><h4 id="amazon-prime">Amazon Prime Video</h4></a>'
+             document.getElementById("amazon-link").setAttribute("href", amazonPrimeTitleUrl);
+          } 
+
+          const hboMax = sources.filter(source => source.source_id === 387 )
+          if (hboMax.length >= 1) {
+            let hboTitleUrl = hboMax[0].web_url
+            document.getElementById("streaming-links").innerHTML +='<a href="" target="_blank" id="hbo-max-link"><h4 id="hbo-max">HBO Max</h4></a>'
+            document.getElementById("hbo-max-link").setAttribute("href", hboTitleUrl);
+          } 
+          // log streaming source data
+          console.log(disneyPlus);
+          console.log(netflix);
+          console.log(hulu);
+          console.log(amazonPrime);
+          console.log(hboMax);
+
+          // feature title data
           var featureTitle = data.title;
           var featureId = data.id;
           var featureImbdId = data.imdb_id;
@@ -49,10 +95,13 @@ var displaySelectedTitle = function(genre) {
           // append feature title data to final page display
           document.getElementById("feature-title").textContent=featureTitle;
           document.getElementById("main-feature-title").textContent=featureTitle;
-          document.getElementById("feature-year").textContent=featureYear;
+          document.getElementById("feature-year").textContent=featureYear +"     ";
           document.getElementById("feature-user-rating").textContent=featureUserRating;
           document.getElementById("feature-critic-score").textContent=featureCriticScore;
-          document.getElementById("feature-rating").textContent=featureRating;
+          document.getElementById("feature-rating").textContent="     Rated: " +featureRating;
+          if (featureRating === null) {
+            document.getElementById("feature-rating").textContent="     Rated: Not Rated";
+          };
           document.getElementById("feature-runtime-minutes").textContent=featureRunTimeMinutes;
           document.getElementById("feature-genre").textContent=featureGenre +" - "+ featureGenre2+" - "+featureGenre3;
           if (featureGenre3 === undefined) {
@@ -67,25 +116,22 @@ var displaySelectedTitle = function(genre) {
           document.getElementById("feature-hero").style.backgroundImage = 'url(' + featureBackdropUrl;
           var trailerThumbnail = document.getElementById("trailer-thumbnail");
           trailerThumbnail.setAttribute("src", featureTrailerThumbnail);
-         // trailerThumbnail.setAttribute("href", featureTrailerUrl)
           document.getElementById("trailer-link").setAttribute("href", featureTrailerUrl);
           document.getElementById("trailer-link").setAttribute("target", "_blank");
-         // call movie database for cast list
+
+       // call movie database for cast list
           var castURL = "https://api.themoviedb.org/3/movie/" + featureImbdId + "/credits?api_key=921ba47b5c4b85bc48dd2db9202db1be&language=en-US"; 
           fetch(castURL).then(function (response) {
           if (response.ok) { 
             response.json().then(function (data) { 
             console.log(data); 
-            var director = Object.values(data.crew[1])[4]; 
+         // var director = Object.values(data.crew[1])[4]; 
             var cast = Object.values(data.cast[0])[4] + ", " + Object.values(data.cast[1])[4] + ", " + Object.values(data.cast[2])[4] + ", " + Object.values(data.cast[3])[4] + ", " + Object.values(data.cast[4])[4] + ", and " + Object.values(data.cast[5])[4]; 
-            // var cast1 = Object.values(data.cast[1])[4];
-            // console.log(cast1);
             console.log(cast);
-            console.log(director);
-          //  document.getElementById("feature-director").textContent = director;
+         // console.log(director);
+         // document.getElementById("feature-director").textContent = director;
             document.getElementById("feature-actors").textContent = cast;
-           // document.getElementById("feature-actors").textContent = cast1;
-           });
+            });
           }
         });
       })
